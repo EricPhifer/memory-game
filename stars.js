575117a97ca8c50a9f1e978d4fe103a0de6944de
Eric <38338180+EriconCode@users.noreply.github.com>
2018-05-24 22:16:25 -0600

    Delete stars.js

--- stars.js
+++
@@ -1,9 +0,0 @@
-var moves = document.getElementsById("moves");
-var stars3 = document.getElementsById("stars3");
-var stars2 = document.getElementsById("stars2");
-
-if (moves = 20) {
-	moves.remove(stars3);
-} else if (moves >= 30) {
-	moves.remove(stars2);
-} 
