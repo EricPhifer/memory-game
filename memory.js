093cff36c452e58a655f62b8424bdacf17431825
Eric <38338180+EriconCode@users.noreply.github.com>
2018-06-04 19:47:05 -0600

    Add files via upload

---
+++ memory.js
@@ -0,0 +1,31 @@
+/* tracks every second card selection and moves the counter up one */
+
+
+
+/* sets timer to start on the first click of card element */
+
+
+
+/* reduces stars by one after 15 moves [2 stars] */
+
+
+
+/* reduces stars by one after 20 moves [1 stars] */
+
+
+
+/* changes star to lemon after 25 moves [0 stars] */
+
+
+
+/* sets the reset button to reset the game on click */
+
+
+
+/* changes the cards to flip on click instead of hover */
+
+
+
+/* sets the modal pop-up upon winning the game */
+
+
